2dc2be189c9ecd87f4a1265c19cb2bf4965cfd58
zerorains <linjunlu@zerorains.top>
2024-03-19 10:45:22 +0800

    add the c++ udf for test

---
+++ examples/embedded-c++/udf.cpp
@@ -0,0 +1,43 @@
+#include "duckdb.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace duckdb;
+
+template <typename TYPE, int NUM_INPUT>
+static void udf_tmp(DataChunk &input, ExpressionState &state, Vector &result) {
+	result.SetVectorType(VectorType::FLAT_VECTOR);
+	auto result_data = ConstantVector::GetData<TYPE>(result);
+	input.Flatten();
+	auto tmp_data1 = ConstantVector::GetData<TYPE>(input.data[0]);
+	auto tmp_data2 = ConstantVector::GetData<TYPE>(input.data[1]);
+	memset(result_data, std::numeric_limits<TYPE>::min(), input.size() * sizeof(TYPE));
+	for (idx_t i = 0; i < input.size(); i++) {
+		result_data[i] = 1 * tmp_data1[i]/2 + 0 * tmp_data2[i];
+	}
+}
+
+void create_data(Connection con, int n = 10000) {
+	std::stringstream ss;
+	ss << "INSERT INTO data VALUES (1, 10)";
+	for (int i = 2; i <= n; i++) {
+		ss << ", (";
+		ss << i;
+		ss << ", ";
+		ss << i * 10;
+		ss << ")";
+	}
+	con.Query(ss.str());
+}
+
+int main() {
+	DuckDB db(nullptr);
+	Connection con(db);
+	con.Query("CREATE TABLE data (i INTEGER, age INTEGER)");
+	create_data(con);
+	con.CreateVectorizedFunction<int, int, int>("udf_vectorized_int", &udf_tmp<int, 2>);
+	con.Query("SELECT udf_vectorized_int(i, age) as res FROM data WHERE i%2==0")->Print();
+	// con.Query("SELECT i FROM data WHERE i%2==0")->Print();
+	return 0;
+}
